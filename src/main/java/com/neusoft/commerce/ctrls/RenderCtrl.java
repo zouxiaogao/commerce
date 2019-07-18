30396d8be7ca40ee7d2eb97fd9c7a9fb96fcf5e5
lucas <721795751@qq.com>
2019-07-18 15:07:21 +0800

    add renderctrl

---
+++ src/main/java/com/neusoft/commerce/ctrls/RenderCtrl.java
@@ -0,0 +1,30 @@
+package com.neusoft.commerce.ctrls;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @Author zqy
+ * @Date 2019/07/18
+ */
+@Controller
+public class RenderCtrl {
+//    @RequestMapping("/admin")
+//    public ModelAndView admin(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse){
+//        AdminInfo admin = this.getAdminInfoFromSession(httpServletRequest);
+//        if(admin == null){
+//            try {
+//                httpServletResponse.sendRedirect("/login/admin");
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//            return null;
+//        }
+//        return this.render("admin",null);
+//    }
+}
