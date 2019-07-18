8a2225c910397c6e223eb642969cb846af902e05
lucas <721795751@qq.com>
2019-07-18 14:13:49 +0800

    commerce

---
+++ src/main/java/com/neusoft/commerce/MybatisConfig.java
@@ -0,0 +1,56 @@
+package com.neusoft.commerce;
+import com.alibaba.druid.pool.DruidDataSource;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+
+/**
+ * @Author zqy
+ * @Date 2019/07/18
+ */
+
+@Configuration
+@MapperScan("com.neusoft.commerce.dao")
+public class MybatisConfig {
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClassName;
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Bean
+    public DruidDataSource dataSource() {
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setDriverClassName(this.driverClassName);
+        dataSource.setUrl(this.url);
+        dataSource.setUsername(this.username);
+        dataSource.setPassword(this.password);
+        dataSource.setInitialSize(5);
+        dataSource.setMaxActive(30);
+        dataSource.setMinIdle(5);
+        dataSource.setMaxWait(60000);
+        return dataSource;
+    }
+
+    @Bean(name="sqlSessionFactory")
+    public SqlSessionFactoryBean sqlSessionFactory(DataSource dataSource) throws IOException {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource);
+        sqlSessionFactoryBean.setConfigLocation(new ClassPathResource("mybatis-config.xml"));
+        sqlSessionFactoryBean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath*:mapper/_*Mapper.xml"));
+        return sqlSessionFactoryBean;
+    }
+}
